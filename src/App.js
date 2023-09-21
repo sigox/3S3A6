dbf0b57ebe0d726d65f44e7ff913edfdd11b8e10
sigox <nina870133@gmail.com>
2023-09-22 02:01:50 +0800

    rewrite

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import './App.css';
 import StepProgess from './components/StepProgess';
 import Step1 from './components/Step1';
@@ -8,11 +8,17 @@ import Cart from './components/cart';
 import ProgressControl from './components/ProgressControl';
 
 export default function App() {
-  
+  const[page,setPage]=useState(1)
+    function nextClick(){
+        setPage(page+1)
+    }
+    function prevClick(){
+        setPage(page-1)
+    }
   return (
     <main className="site-main">
       <div className="main-container">
-        <section className="register-container col col-lg-6 col-sm-12" data-phase="1" data-total-price="0">
+        <section className="register-container col col-lg-6 col-sm-12" data-phase={page} data-total-price="0">
           <h2 className="register-title col col-12">結帳</h2>
           <StepProgess />
           <section className="form-container col col-12">
@@ -22,7 +28,7 @@ export default function App() {
           </section>
         </section>
         <Cart />
-        <ProgressControl />
+        <ProgressControl nextClick={nextClick} prevClick={prevClick}/>
       </div>
     </main>
   );
